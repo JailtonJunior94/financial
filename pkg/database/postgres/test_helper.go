946cf6819b450f1d9670e7e94e1fb280e0aa517b
JailtonJunior94 <jailton.junior94@outlook.com>
2024-02-21 20:27:02 -0300

    adicionado testcontainers no projeto

---
+++ pkg/database/postgres/test_helper.go
@@ -0,0 +1,49 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"time"
+
+	migration "github.com/jailtonjunior94/financial/pkg/database/migrate"
+
+	"github.com/testcontainers/testcontainers-go"
+	postgresContainer "github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+type PostgresContainer struct {
+	Container *postgresContainer.PostgresContainer
+}
+
+func NewPostgresContainer(ctx context.Context) *PostgresContainer {
+	postgres, err := postgresContainer.RunContainer(
+		ctx,
+		testcontainers.WithImage("postgres:16.2-alpine"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second),
+		),
+		postgresContainer.WithDatabase("tramporteiro"),
+		postgresContainer.WithUsername("postgres"),
+		postgresContainer.WithPassword("postgres"),
+	)
+
+	if err != nil {
+		log.Fatalf("could not start postgres container: %s", err)
+	}
+	return &PostgresContainer{Container: postgres}
+}
+
+func (s *PostgresContainer) ExecuteMigration(db *sql.DB, dbName, migratePath string) {
+	migrate, err := migration.NewMigrate(nil, db, migratePath, dbName)
+	if err != nil {
+		log.Fatalf("could not start migrate: %s", err)
+	}
+
+	if err = migrate.ExecuteMigration(); err != nil {
+		log.Fatalf("could execute migration: %s", err)
+	}
+}
